228eed0590bb0ae0eff18719980513628f64468e
hllibrkaya <hkaya4040@outlook.com>
2022-09-27 23:42:50 +0300

    Exercises2

---
+++ Methods/src/Main.java
@@ -0,0 +1,18 @@
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		sum(3,5);
+		
+	}
+	
+	public static void sum( int num1, int num2) {
+		
+		System.out.println(num1 + num2);
+	}
+
+	
+
+}

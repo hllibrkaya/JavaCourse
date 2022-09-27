75b9b1f7f345d85ad639300b436f684d60e6afae
hllibrkaya <hkaya4040@outlook.com>
2022-09-28 00:49:30 +0300

    Exercises2

---
+++ Encapsulation/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Student s1= new Student();
+		System.out.println(s1.getClassroom());
+		s1.setName("Halil");
+		System.out.println(s1.getName());
+
+	}
+
+}
---
+++ Encapsulation/src/Student.java
@@ -0,0 +1,25 @@
+
+public class Student {
+	
+	private String name;
+	private String surname;
+	private int age;
+	private String id;
+	private String classroom= "4th";
+	private String gender;
+	
+	public String getClassroom() {
+		return classroom;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+}

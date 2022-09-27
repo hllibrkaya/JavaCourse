0770be6819d7ba14232567ac3f35e76715d97f2a
hllibrkaya <hkaya4040@outlook.com>
2022-09-28 00:12:24 +0300

    Exercises2

---
+++ Classes/src/CustomerAccount.java
@@ -0,0 +1,18 @@
+
+public class CustomerAccount {
+	
+	String name;
+	String surname;
+	double money;
+	int age;
+	
+	
+	public void addMoney(int num) {
+		money +=num;
+	}
+	
+	public void subMoney(int num) {
+		money -=num;
+	}
+
+}
---
+++ Classes/src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		CustomerAccount s1= new CustomerAccount();
+		s1.age=23;
+		s1.name="Halil";
+		s1.surname="Kaya";
+		s1.addMoney(100);
+		
+		System.out.println(s1.money);
+
+	}
+
+}

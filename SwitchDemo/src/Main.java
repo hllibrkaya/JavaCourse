6b4b38c1cf7bab0ebfd9702f7e116705ad792817
hllibrkaya <hkaya4040@outlook.com>
2022-09-27 21:12:00 +0300

    Exercises

---
+++ SwitchDemo/src/Main.java
@@ -0,0 +1,33 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		char grade='F';
+		
+		switch(grade) {
+		
+		case 'A':
+			System.out.println("Well done, You passed");
+			break;
+			
+		case 'B':
+			System.out.println("Well done, You passed");
+			break;
+			
+		case 'C':
+			System.out.println("Well done, You passed");
+			break;
+			
+		case 'D':
+			System.out.println("Well done, You passed");
+			break;
+		
+		case 'F':
+			System.out.println("Sorry, You failed");
+			break;
+		}
+
+	}
+
+}

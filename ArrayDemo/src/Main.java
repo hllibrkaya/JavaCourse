9738ca093a400f32ba9cc166d9cb3b5bcebd0d62
hllibrkaya <hkaya4040@outlook.com>
2022-09-27 21:35:33 +0300

    Exercises

---
+++ ArrayDemo/src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int grades[]= new int[3];
+		grades[0]=94; 
+		grades[1]=50;
+		grades[2]=72;
+		
+		
+		for (int i : grades) {
+			System.out.println(i);
+		}
+		}
+	}
+
+

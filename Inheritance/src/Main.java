53ceba3e294554cb4c5499cb2bec7b8b9c24ad88
hllibrkaya <hkaya4040@outlook.com>
2022-09-28 01:21:25 +0300

    Exercises2

---
+++ Inheritance/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Customer c1= new Customer();
+		Employee e1= new Employee();
+		
+		c1.name="Halil";
+		e1.name="Jack";
+		e1.salary=15.238;
+		System.out.println(e1.salary);
+
+	}
+
+}
---
+++ Inheritance/src/Person.java
@@ -0,0 +1,10 @@
+
+public class Person {
+	public String name;
+	public String surname;
+	public int age;
+	public String id;
+	public String gender;
+	
+
+}

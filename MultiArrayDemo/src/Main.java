038738292650a39ad84d1fc942a1dc3e7a39fd8a
hllibrkaya <hkaya4040@outlook.com>
2022-09-27 21:54:35 +0300

    Exercises

---
+++ MultiArrayDemo/src/Main.java
@@ -0,0 +1,23 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		String[][] cities= new String[2][2];
+		cities[0][0]= "Ankara";
+		cities[0][1]= "Kırşehir";
+		cities[1][0]= "Adana";
+		cities[1][1]= "Mersin";
+		
+		for (int i = 0; i <=1; i++) {
+			for (int j = 0; j <=1; j++) {
+				
+				System.out.println(cities[i][j]);
+			}
+			
+		}
+			
+		}
+	}
+
+

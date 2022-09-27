5febbb7822d9d2ea50adab1a87b7df5d2d979117
hllibrkaya <hkaya4040@outlook.com>
2022-09-28 01:08:46 +0300

    Exercises2

---
+++ MethodOverloading/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Operations o1= new Operations();
+		o1.sum(2.5, 3);
+		o1.sum(2, 2);
+		o1.sum(3, 5, 7);
+		
+		
+	}
+
+}
---
+++ MethodOverloading/src/Operations.java
@@ -0,0 +1,16 @@
+
+public class Operations {
+	
+	public void sum(int num1, int num2) {
+		System.out.println(num1+num2);
+	}
+	
+	public void sum(double num1, double num2) {
+		System.out.println(num1+num2);
+	}
+	
+	public void sum(int num1, int num2, int num3) {
+		System.out.println(num1+num2+num3);
+	}
+
+}
